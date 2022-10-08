36cd7fe2493eec3526e27fb88d4be01131ffdc3c
tergytrg <73442558+tergytrg@users.noreply.github.com>
2022-10-08 22:41:21 +0200

    Libraries

--- src/main/java/com/example/osu_parse/MainController.java
+++ src/main/java/com/example/osu_parse/MainController.java
@@ -1,10 +1,30 @@
 package com.example.osu_parse;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class MainController {
 
     @FXML
+    private GridPane rootPane;
+
+    public void initialize() {
+
+    }
+
+    @FXML
     protected void onParse() {
+    }
+
+    @FXML
+    protected void selectPath() {
+        Stage stage = (Stage) rootPane.getScene().getWindow();
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Please select output folder");
+        File selectedDirectory = directoryChooser.showDialog(stage);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.osu_parse {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.poi.ooxml;
+    requires org.json.simple;
 
 
     opens com.example.osu_parse to javafx.fxml;
